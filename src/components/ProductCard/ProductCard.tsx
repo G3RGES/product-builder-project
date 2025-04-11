2908c21172daad6d4ec742ee2d21b3fd419528f2
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-04-11 21:36:20 +0200

    Create ProductCard.tsx

---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface IProps {}
+
+const ProductCard = ({}: IProps) => {
+  return (
+    <div className="border-2 rounded-md">
+      <span className="">product card</span>
+    </div>
+  );
+};
+
+export default ProductCard;
