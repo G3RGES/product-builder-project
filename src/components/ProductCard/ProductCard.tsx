98cd49b5fd0503b9682327f26db7d2d2bacfe57d
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-04-12 08:11:21 +0200

    Update ProductCard.tsx

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -4,8 +4,35 @@ interface IProps {}
 
 const ProductCard = ({}: IProps) => {
   return (
-    <div className="border-2 rounded-md">
-      <span className="">product card</span>
+    <div className="border rounded-md p-2 flex flex-col">
+      <img className="" src="" alt="product_name" />
+      <h3 className=""></h3>
+      <p className=""></p>
+      <div className="flex items-center space-x-1.5 my-2">
+        <span className="w-5 h-5 bg-black rounded-full cursor-pointer" />
+        <span className="w-5 h-5 bg-red-600 rounded-full cursor-pointer" />
+        <span className="w-5 h-5 bg-blue-800 rounded-full cursor-pointer" />
+      </div>
+
+      <div className="flex items-center justify-between">
+        <span className=""></span>
+        <img className="w-10 h-10 rounded-full" src="" alt="" />
+      </div>
+
+      <div className="flex items-center justify-between space-x-1.5 my-5">
+        <button
+          className="bg-green-700 text-white p-2 w-full
+         rounded-md font-semibold "
+        >
+          Edit
+        </button>
+        <button
+          className="bg-red-700 text-white p-2 w-full
+         rounded-md font-semibold "
+        >
+          Delete
+        </button>
+      </div>
     </div>
   );
 };
