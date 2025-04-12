0b1f212430706b7508ed38d89362f656fa8697c0
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-04-12 08:38:45 +0200

    Update Button.tsx

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -5,7 +5,14 @@ interface IProps {
 }
 
 const Button = ({ children, className }: IProps) => {
-  return <button className={className}>{children}</button>;
+  return (
+    <button
+      className={`${className} text-white p-2 w-full
+         rounded-md font-semibold`}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
