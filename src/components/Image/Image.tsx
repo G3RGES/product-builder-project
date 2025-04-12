cec430f8a3d053dabe40201a714fe79325d7e0b8
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-04-12 08:26:11 +0200

    Update Image.tsx

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -3,10 +3,11 @@ import React from "react";
 interface IProps {
   imageURL: string;
   alt: string;
+  className: string;
 }
 
-const Image = ({ imageURL, alt }: IProps) => {
-  return <img className="" src={imageURL} alt={alt} />;
+const Image = ({ imageURL, alt, className }: IProps) => {
+  return <img className={className} src={imageURL} alt={alt} />;
 };
 
 export default Image;
