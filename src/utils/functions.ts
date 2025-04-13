e8a7dca2ec1cb05542d0cf89890c13b0c16d6edb
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-04-13 15:14:42 +0200

    Update functions.ts

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,3 +1,10 @@
+/**
+ *
+ * @param {string} text - the text to be sliced
+ * @param {number} [limit = 50] - the maximum length before truncation
+ * @returns - the sliced text
+ */
+
 export function txtSlicer(text: string, limit: number = 50) {
   if (text.length >= limit) return `${text.slice(0, limit)} ...`;
   return text;
