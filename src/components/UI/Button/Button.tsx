c1d15872d22699a647941f3710a078dec617fb1e
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-04-13 10:17:09 +0200

    Update Button.tsx

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -2,12 +2,13 @@ import React, { ButtonHTMLAttributes, ReactNode } from "react";
 interface IProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   className?: string;
+  width?: "w-full" | "w-fit";
 }
 
-const Button = ({ children, className, ...rest }: IProps) => {
+const Button = ({ children, className, width = "w-full", ...rest }: IProps) => {
   return (
     <button
-      className={`${className} text-white p-2 w-full
+      className={`${className} ${width} text-white p-2 w-full
          rounded-md font-semibold cursor-pointer`}
       {...rest}
     >
