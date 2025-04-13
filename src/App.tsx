8c185fb01d511f951b5b7cde4669a05636f09115
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-04-13 18:39:47 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ import { productList } from "./data";
 function App() {
   return (
     <div className="container mx-auto">
-      <div className="grid grid-cols-4 gap-2 ">
+      <div
+        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4
+       gap-2 p-2 rounded-md  "
+      >
         {productList.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
