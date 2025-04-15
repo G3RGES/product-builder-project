b5433297e664a0312d0ce310ea9cba5e9bf0e3b6
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-04-15 10:45:12 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 import ProductCard from "./components/ProductCard/ProductCard";
+import Modal from "./components/UI/Modal/Modal";
 import { productList } from "./data";
+import Button from "./components/UI/Button/Button";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  //* MODAL HANDLER *//
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
     <div className="container mx-auto">
+      <Button className="bg-green-700 hover:bg-green-600" onClick={openModal}>
+        Add
+      </Button>
+
       <div
         className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4
        gap-2 p-2 rounded-md  "
@@ -13,6 +31,13 @@ function App() {
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
+      <Modal isOpen={isOpen} closeModal={closeModal} title="Add New Product">
+        <div className="flex items-center justify-between space-x-2.5">
+          <Button className="bg-green-700 hover:bg-green-600">Submit</Button>
+
+          <Button className="bg-gray-400 hover:bg-gray-500 ">Cancel</Button>
+        </div>
+      </Modal>
     </div>
   );
 }
