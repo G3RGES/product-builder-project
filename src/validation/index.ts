7150cdd7a2533bb967394812018333ded9bbb106
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-04-16 15:33:52 +0200

    Update index.ts

---
+++ src/validation/index.ts
@@ -0,0 +1,29 @@
+/**
+ * Validates a product object for required fields and constraints.
+ *
+ * @param {Object} product - The product to be validated.
+ * @param {string} product.title - The title of the product.
+ * @param {string} product.description - The description of the product.
+ * @param {string} product.imageURL - The URL of the product's image.
+ * @param {string} product.price - The price of the product.
+ *
+ * @returns {Object} - An object containing error messages for invalid fields.
+ * @property {string} title - Error message for the title field.
+ * @property {string} description - Error message for the description field.
+ * @property {string} imageURL - Error message for the imageURL field.
+ * @property {string} price - Error message for the price field.
+ */
+
+export const productValidation = () => {
+  const errors: {
+    title: string;
+    description: string;
+    imageURL: string;
+    price: string;
+  } = {
+    title: "",
+    description: "",
+    imageURL: "",
+    price: "",
+  };
+};
