b5cdd00b4435fa6dcfe668d62d0c5e025d2489a4
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-04-16 11:37:28 +0200

    Update Input.tsx

--- src/components/UI/Input/Input.tsx
+++ src/components/UI/Input/Input.tsx
@@ -2,7 +2,13 @@ import React, { InputHTMLAttributes } from "react";
 
 interface IProps extends InputHTMLAttributes<HTMLInputElement> {}
 const Input = ({ ...rest }: IProps) => {
-  return <input {...rest} />;
+  return (
+    <input
+      className="border-[1px] border-gray-300 shadow-md focus:border-[#034694] focus:outline-none
+      focus:ring-1 focus:ring-[#034694] rounded-md px-3 py-3 text-md "
+      {...rest}
+    />
+  );
 };
 
 export default Input;
