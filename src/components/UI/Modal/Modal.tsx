fbcf05a29456ee09b5cb562e7770373cddd81e9f
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-04-16 11:37:30 +0200

    Update Modal.tsx

--- src/components/UI/Modal/Modal.tsx
+++ src/components/UI/Modal/Modal.tsx
@@ -29,12 +29,14 @@ const Modal = ({ isOpen, closeModal, title, children }: IProps) => {
           <div className="flex min-h-full items-center justify-center p-4">
             <DialogPanel
               transition
-              className="w-full max-w-md rounded-xl bg-black p-6 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
+              className="w-full max-w-md rounded-xl bg-white p-6 backdrop-blur-2xl
+               duration-300 ease-out data-[closed]:transform-[scale(95%)]
+                data-[closed]:opacity-0"
             >
               {title && (
                 <DialogTitle
                   as="h3"
-                  className="text-base/7 font-medium text-white"
+                  className="text-base/7 font-medium text-black text-center text-xl"
                 >
                   {title}
                 </DialogTitle>
