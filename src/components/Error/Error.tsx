3b051a86dcf871d54244bd1dbe3031cd2ed7d2f1
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-04-17 12:23:25 +0200

    Update Error.tsx

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -6,7 +6,9 @@ interface IProps {
 
 const Error = ({ msg }: IProps) => {
   return (
-    <span className="block text-red-700 font-semibold text-sm">{msg}</span>
+    msg && (
+      <span className="block text-red-700 font-semibold text-sm">{msg}</span>
+    )
   );
 };
 
