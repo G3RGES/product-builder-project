7e0fb3b402b864ee684f7657f0d8b4fe47cf2781
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-04-17 12:52:09 +0200

    Update CircleColor.tsx

--- src/components/CircleColor/CircleColor.tsx
+++ src/components/CircleColor/CircleColor.tsx
@@ -3,7 +3,7 @@ import React from "react";
 interface IProps {}
 
 const CircleColor = ({}: IProps) => {
-  return <div></div>;
+  return <span className="w-5 h-5 bg-black rounded-full cursor-pointer" />;
 };
 
 export default CircleColor;
