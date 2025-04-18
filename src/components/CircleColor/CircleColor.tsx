6d41dd2eb85eab7e9e6d928b304a60b45d9b5f0d
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-04-18 13:29:17 +0200

    Update CircleColor.tsx

--- src/components/CircleColor/CircleColor.tsx
+++ src/components/CircleColor/CircleColor.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 
-interface IProps {
+interface IProps extends React.HTMLAttributes<HTMLSpanElement> {
   color: string;
+  onClick?: () => void;
 }
 
-const CircleColor = ({ color }: IProps) => {
+const CircleColor = ({ color, onClick }: IProps) => {
   return (
     <span
       className={`w-5 h-5  rounded-full cursor-pointer`}
       style={{ backgroundColor: color }}
+      onClick={onClick}
     />
   );
 };
