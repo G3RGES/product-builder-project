04d77a156974dfd0d8423d0482f0f1e0a1a03c72
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-04-24 15:05:33 +0200

    Update Modal.tsx

--- src/components/UI/Modal/Modal.tsx
+++ src/components/UI/Modal/Modal.tsx
@@ -1,52 +1,75 @@
-import React from "react";
-import { Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
+import {
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+  Transition,
+  TransitionChild,
+} from "@headlessui/react";
+import { Fragment, ReactNode } from "react";
 
 interface IProps {
   isOpen: boolean;
   closeModal: () => void;
   title?: string;
-  children: React.ReactNode;
+  description?: string;
+  children: ReactNode;
 }
 
-const Modal = ({ isOpen, closeModal, title, children }: IProps) => {
+const Modal = ({
+  isOpen,
+  closeModal,
+  title,
+  description,
+  children,
+}: IProps) => {
   return (
     <>
-      {/* <Button
-        onClick={open}
-        className="rounded-md bg-[#034694] py-2 px-4 text-sm
-         font-medium text-white cursor-pointer"
-      >
-        Open dialog
-      </Button> */}
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <div className="fixed inset-0 backdrop-blur-sm" aria-hidden="true" />
+          <TransitionChild
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0  bg-opacity-25" />
+          </TransitionChild>
 
-      <Dialog
-        open={isOpen}
-        as="div"
-        className="relative z-10 focus:outline-none"
-        onClose={closeModal}
-      >
-        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-          <div className="flex min-h-full items-center justify-center p-4">
-            <DialogPanel
-              transition
-              className="w-full max-w-md rounded-xl bg-white p-6 backdrop-blur-2xl
-               duration-300 ease-out data-[closed]:transform-[scale(95%)]
-                data-[closed]:opacity-0"
-            >
-              {title && (
-                <DialogTitle
-                  as="h3"
-                  className="font-medium text-black text-center text-xl"
-                >
-                  {title}
-                </DialogTitle>
-              )}
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <TransitionChild
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <DialogPanel className="w-full max-w-md transform overflow-hidden rounded-lg bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  {title && (
+                    <DialogTitle
+                      as="h3"
+                      className="text-lg font-medium leading-6 text-gray-900"
+                    >
+                      {title}
+                    </DialogTitle>
+                  )}
+                  {description && (
+                    <p className="text-sm text-gray-500 mt-3">{description}</p>
+                  )}
 
-              <div className="mt-4">{children}</div>
-            </DialogPanel>
+                  <div className="mt-4">{children}</div>
+                </DialogPanel>
+              </TransitionChild>
+            </div>
           </div>
-        </div>
-      </Dialog>
+        </Dialog>
+      </Transition>
     </>
   );
 };
