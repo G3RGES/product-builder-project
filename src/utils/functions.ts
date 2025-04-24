7f285e55b7bce0eaa707bef2ad63acc9e1930386
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-04-24 14:45:33 +0200

    Update functions.ts

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -9,3 +9,13 @@ export function txtSlicer(text: string, limit: number = 50) {
   if (text.length >= limit) return `${text.slice(0, limit)} ...`;
   return text;
 }
+
+/**
+ *
+ * @param {string} x - The numeric string to be formatted.
+ * @returns {string} A formatted version of the input numeric string with commas as thousand separators.
+ *
+ */
+export function numberWithCommas(x: string): string {
+  return x?.toString()?.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
